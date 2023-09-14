155deae4ff5e5c6b0263cd532310a2f2a3f3aefb
AxelLuxem <104254539+AxelLuxem@users.noreply.github.com>
2023-09-14 18:13:01 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { NavBar } from "./components/NavBar.jsx";
+
+export const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>Welcome to CarBuy</h1>
+    </div>
+  );
+};
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+export const NavBar = () => {
+  return (
+    <ul>
+      <li>
+        <a href="">Random</a>
+      </li>
+    </ul>
+  );
+};
